a49d20398efc7ac919b674545699d186ac58e3a6
makerstoday <marcelobarbosafb@gmail.com>
2023-02-04 00:34:52 -0300

    js

---
+++ scipt.js
@@ -0,0 +1,10 @@
+
+//condição simples apenas com if
+
+var vel = 80.9
+console.log(`marcello ${vel} km/h`)
+if(vel < 80) { // condição simples
+    console.log(`vc ultrapassou a velocidade permitida`)
+    console.log(`volte a velocidade normal permitida`)
+}
+console.log(`dirija com segurança`)
---
+++ script1.js
@@ -0,0 +1,10 @@
+//condição
+
+var estado = "maranhão"
+console.log(`vivendo no ${estado}`)
+if(estado == "para"){
+    console.log(` você é maranhense`)
+}else{
+    console.log(`vc é forasteiro`)
+}
+
